8e8869f7b3c1b3fae79ade914d970872ea3344c8
Thai Luc <thaitangluc2412@gmail.com>
2021-12-28 15:15:31 +0700

    java

---
+++ ThucTap/src/main/java/persistence/Account.java
@@ -0,0 +1,20 @@
+package persistence;
+
+public class Account {
+	private String userID;
+	private String userName;
+	private String userPassword;
+	private String name;
+	private String address;
+	private String phoneNumber;
+	private String Mail;
+	private String AccountNumber;
+	private Role role;
+	
+	public Account() {
+	}
+
+	
+	
+	
+}
--- ThucTap/src/model/Book_Detail.java
+++
@@ -1,7 +0,0 @@
-package model.bean;
-import java.time.LocalDateTime;
-
-public class Book_Detail {
-	private int UserID;
-	private int BookID;
-}
